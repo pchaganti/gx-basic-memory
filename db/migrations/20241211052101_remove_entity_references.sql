0faf70d2e996300cdc5861633621edeca8f90b4f
phernandez <phernandez@basicmachines.co>
2024-12-21 18:04:36 -0600

    remove unused files

--- db/migrations/20241211052101_remove_entity_references.sql
+++
@@ -1,5 +0,0 @@
--- migrate:up
-ALTER TABLE entity DROP COLUMN "references";
-
--- migrate:down
-ALTER TABLE entity ADD COLUMN "references" TEXT DEFAULT NULL;
--- db/migrations/20241211190000_add_entity_type_name_unique.sql
+++
@@ -1,8 +0,0 @@
--- migrate:up
--- Add unique index on entity type and name combination
-CREATE UNIQUE INDEX idx_entity_type_name ON entity(entity_type, name);
-
--- migrate:down
-
--- Restore original schema
-DROP INDEX IF EXISTS idx_entity_type_name;
